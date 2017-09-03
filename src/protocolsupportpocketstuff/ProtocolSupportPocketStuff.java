5dd04323c7b647e816adb5e50ef1898554b00928
7kasper <kasper77077@gmail.com>
2017-09-03 21:01:39 +0200

    Add listener. Still need to do everything.

--- src/protocolsupportpocketstuff/ProtocolSupportPocketStuff.java
+++ src/protocolsupportpocketstuff/ProtocolSupportPocketStuff.java
@@ -4,10 +4,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import protocolsupportpocketstuff.listeners.SkinListener;
+
 public class ProtocolSupportPocketStuff extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
+		getServer().getPluginManager().registerEvents(new SkinListener(), this);
 		Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_PURPLE + "Hello World!" + ChatColor.RESET);
 	}
 	
---
+++ src/protocolsupportpocketstuff/listeners/SkinListener.java
@@ -0,0 +1,9 @@
+package protocolsupportpocketstuff.listeners;
+
+import org.bukkit.event.Listener;
+
+public class SkinListener implements Listener {
+	
+	//TODO: MAKE EVERYTHING :P
+	
+}
