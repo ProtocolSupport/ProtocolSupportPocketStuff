af131aa6f6f26a31b06b8a6168543f18173d56cf
Leonardo Malaman <leonardomalaman@gmail.com>
2017-11-01 17:51:01 -0200

    Spaces -> TABs

--- src/protocolsupportpocketstuff/api/util/DeviceOperatingSystem.java
+++ src/protocolsupportpocketstuff/api/util/DeviceOperatingSystem.java
@@ -2,16 +2,16 @@ package protocolsupportpocketstuff.api.util;
 
 public enum DeviceOperatingSystem {
 	ANDROID(1),
-    IOS(2),
-    OSX(3),
-    FIRE_OS(4),
-    GEAR_VR(5),
-    HOLOLENS(6),
-    WINDOWS_10(7),
-    WINDOWS(8),
-    DEDICATED(9),
-    ORBIS(10),
-    NINTENDO_SWITCH(11),
+	IOS(2),
+	OSX(3),
+	FIRE_OS(4),
+	GEAR_VR(5),
+	HOLOLENS(6),
+	WINDOWS_10(7),
+	WINDOWS(8),
+	DEDICATED(9),
+	ORBIS(10),
+	NINTENDO_SWITCH(11),
 	UNKNOWN(-1);
 
 	private int id;
